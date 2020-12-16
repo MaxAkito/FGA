a35e70d69889d17dab47ad407a1b4705a8ea68bd
Max <10167995+MaxAkito@users.noreply.github.com>
2020-12-16 02:58:15 -0500

    Update GameServerEnum.kt
    Added better FGO JP and NA support

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/enums/GameServerEnum.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/enums/GameServerEnum.kt
@@ -1,8 +1,8 @@
 package com.mathewsachin.fategrandautomata.scripts.enums
 
 enum class GameServerEnum constructor(vararg val packageNames: String) {
-    En("com.aniplex.fategrandorder.en", "com.aniplex.fategrandorder.eo"),
-    Jp("com.aniplex.fategrandorder", "com.aniplex.fategrandordes"),
+    En("com.aniplex.fategrandorder.en", "com.aniplex.fategrandorder.eo", "io.rayshift.betterfgo.en"),
+    Jp("com.aniplex.fategrandorder", "com.aniplex.fategrandordes", "io.rayshift.betterfgo"),
     Cn("com.bilibili.fatego", "com.bilibili.fatego.sharejoy"),
     Tw("com.komoe.fgomycard", "com.xiaomeng.fategrandorder"),
     Kr("com.netmarble.fgok");
