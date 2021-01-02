7501445b25f73e8222c3746e17f46aa46ba30c9d
Max <10167995+MaxAkito@users.noreply.github.com>
2021-01-02 04:04:38 -0500

    Update GameServerEnum.kt

--- scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/enums/GameServerEnum.kt
+++ scripts/src/main/java/com/mathewsachin/fategrandautomata/scripts/enums/GameServerEnum.kt
@@ -1,8 +1,8 @@
 package com.mathewsachin.fategrandautomata.scripts.enums
 
 enum class GameServerEnum constructor(vararg val packageNames: String) {
-    En("com.aniplex.fategrandorder.en", "com.aniplex.fategrandorder.eo", "io.rayshift.betterfgo.en"),
-    Jp("com.aniplex.fategrandorder", "com.aniplex.fategrandordes", "io.rayshift.betterfgo"),
+    En("com.aniplex.fategrandorder.en", "com.aniplex.fategrandorder.eo", "io.rayshift.betterfgo.en", "io.rayshift.betterfgo.enx"),
+    Jp("com.aniplex.fategrandorder", "com.aniplex.fategrandordes", "io.rayshift.betterfgo", "io.rayshift.betterfgox"),
     Cn("com.bilibili.fatego", "com.bilibili.fatego.sharejoy"),
     Tw("com.komoe.fgomycard", "com.xiaomeng.fategrandorder"),
     Kr("com.netmarble.fgok");
